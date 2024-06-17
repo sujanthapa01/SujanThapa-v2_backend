2c38e9a69bb801688e9290c089a9f17afdd452bd
sujanthapa01 <sujanthapa4001@gmail.com>
2024-06-18 01:32:40 +0530

    c

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+
 const DataBaseConnection= require('./connection.js')
 const app = express();
 const router = require('./Routes/router.js');
 
 const DataBaseURL = process.env.DATA_BASE_URL;
-DataBaseConnection(DataBaseURL).then(() =>{
-	console.log("DataBase Connected sucessfully");
-})
+DataBaseConnection(DataBaseURL).then(() => console.log("DataBase Conneted Sucessfully"))
 
 app.use(express.json());
 app.use('/', router);
 
 
-const PORT = process.env.SERVER_PORT ;
+const PORT = process.env.SERVER_PORT || 8000 ;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
