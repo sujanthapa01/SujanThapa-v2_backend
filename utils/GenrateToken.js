ecdf0fa84ce5412608dc7d042d8d0c7a359e09c9
sujanthapa01 <sujanthapa4001@gmail.com>
2024-06-20 00:07:49 +0530

    commit

--- utils/GenrateToken.js
+++ utils/GenrateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const secretKey = process.env.SECRET_KEY;
 
 const generateToken = (user) => {
-  return jwt.sign({ id: user._id, email: user.email, password: user.password }, secretKey, { expiresIn: '1h' });
+  return jwt.sign({ id: user._id, username : user.username, email: user.email }, secretKey, { expiresIn: '1h' });
 };
 
 module.exports = generateToken;
