02ed4a1aa881588327c81e6e59ac71fc66610dc1
sujanthapa01 <sujanthapa4001@gmail.com>
2024-07-02 15:14:39 +0530

    email template added

--- Routes/router.js
+++ Routes/router.js
@@ -2,14 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const { login, handlenewUser,  handleForgetPassword} = require("../controller/AuthController.js");
-const { renderResetPasswordForm, handleResetPassword } = require('../controller/auth.js');
 const verifyToken = require('../middleware/middleware.js');
 
 router.post('/login', login);
 router.post('/signup', handlenewUser);
 router.post('/forget-password', handleForgetPassword);
-router.get('/reset-password/:token', renderResetPasswordForm);
-router.post('/reset-password/:token', handleResetPassword);
 router.get('/protected-route', verifyToken, (req, res) => {
   res.json({ message: "This is a protected route", user: req.user });
 });
